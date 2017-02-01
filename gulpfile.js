23cf8f368e8ac500e391245b91b95f324200fb76
Cian <cian6390@gmail.com>
2017-02-01 14:02:19 +0800

    basic start

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+let gulp = require('gulp');
+
+gulp.task('default', function() {
+    console.log('Gulp already to go.');
+});
+
+gulp.task('hello', function() {
+    console.log('Hello gulp');
+});
