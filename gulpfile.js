d8bca8f6133dedf446080a29699d3e4753de5223
Cian <cian6390@gmail.com>
2017-02-01 15:43:24 +0800

    include sass and compass

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,17 @@
 let gulp = require('gulp');
+let gulpCompass = require('gulp-compass');
 
-gulp.task('default', function() {
-    console.log('Gulp already to go.');
+gulp.task('watch',function(){
+	// 新增一個watch任務
+    gulp.watch('./src/scss/**/*.scss',['default']);
 });
 
-gulp.task('hello', function() {
-    console.log('Hello gulp');
+gulp.task('default', function () {
+    gulp.src('./src/scss/**/*.scss')  // 任務監聽檔案目錄
+        .pipe(gulpCompass({
+            css: './lib/css',	   // 輸出目錄
+            sass: './src/scss',    // scss目錄
+            image: './src/images', // 圖檔目錄
+            comments: false,	   // 取消註解
+        }));
 });
